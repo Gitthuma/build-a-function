ebc1cbc0798c083cd5727746f5060c2cec713311
Gitthuma <george.githuma@gmail.com>
2021-04-20 22:27:35 +0300

    Used DOM API function called Element.setAttribute() to set a class attribute on our panel with a value of msgBox.

--- code/main.js
+++ code/main.js
@@ -9,6 +9,11 @@ function displayMessage() {
 
     /*Use DOM API function called document.createElement() to create a <div> element and store a reference to it in a constant called panel. This element will be the outer container of our message box.*/
 
+    /*Use DOM API function called Element.setAttribute() to set a class attribute on our panel with a value of msgBox. This is to make it easier to style the element — if you look at the CSS on the page, you'll see that we are using a .msgBox class selector to style the message box and its contents.*/
+
     const panel = document.createElement('div');
+    panel.setAttribute('class', 'msgBox');
+
+    
 
 }
