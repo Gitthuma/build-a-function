722afa31cd6b68c65155ead34864bfb31750f899
Gitthuma <george.githuma@gmail.com>
2021-04-20 22:15:41 +0300

    Used a DOM API function called document.querySelector() to select the <html> element and store a reference to it in a constant called html

--- code/main.js
+++ code/main.js
@@ -1,5 +1,10 @@
+/*The basic function*/
+
 /*We start off with the keyword function, which means we are defining a function. This is followed by the name we want to give to our function, a set of parentheses, and a set of curly braces. Any parameters we want to give to our function go inside the parentheses, and the code that runs when we call the function goes inside the curly braces.*/
 
 function displayMessage() {
+    /*Use a DOM API function called document.querySelector() to select the <html> element and store a reference to it in a constant called html, so we can do things to it later on*/
+
+    const html = document.querySelector('html');
 
 }
